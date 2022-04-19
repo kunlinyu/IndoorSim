61a1a99cae203de3d015c39fa311fb691b9ba9c1
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-04-19 14:46:25 +0800

    Add color to log
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- Assets/src/view/UI/LogWindow.cs
+++ Assets/src/view/UI/LogWindow.cs
@@ -5,9 +5,16 @@ using UnityEngine.UIElements;
 
 public class LogWindow : MonoBehaviour
 {
+
+    private StyleColor logColor;
+    private StyleColor warningColor;
+    private StyleColor errorColor;
     void OnEnable()
     {
         Application.logMessageReceived += HandleLog;
+        logColor = GetComponent<UIDocument>().rootVisualElement.Q<Label>("LogBackground").resolvedStyle.backgroundColor;
+        warningColor = GetComponent<UIDocument>().rootVisualElement.Q<Label>("WarningBackground").resolvedStyle.backgroundColor;
+        errorColor = GetComponent<UIDocument>().rootVisualElement.Q<Label>("ErrorBackground").resolvedStyle.backgroundColor;
     }
 
     void OnDisable()
@@ -18,6 +25,14 @@ public class LogWindow : MonoBehaviour
     void HandleLog(string logString, string stackTrace, LogType type)
     {
         ListView listView = GetComponent<UIDocument>().rootVisualElement.Q<ListView>("LogList");
-        listView.hierarchy.Add(new Label(type.ToString() + "[" + DateTime.Now.TimeOfDay.ToString(@"hh\:mm\:ss\.ff") + "] " + logString));
+
+        Label label = new Label(type.ToString() + "[" + DateTime.Now.TimeOfDay.ToString(@"hh\:mm\:ss\.ff") + "] " + logString);
+        if (type == LogType.Log)
+            label.style.backgroundColor = logColor;
+        else if (type == LogType.Warning)
+            label.style.backgroundColor = warningColor;
+        else if (type == LogType.Error)
+            label.style.backgroundColor = errorColor;
+        listView.hierarchy.Add(label);
     }
 }
