fc76722aa360ea547056e83efc1feea5af736a17
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-04-27 11:24:10 +0800

    Use dynamic bazier handler length
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- Assets/src/model/indoor_tiling/RepresentativeLine.cs
+++ Assets/src/model/indoor_tiling/RepresentativeLine.cs
@@ -32,38 +32,42 @@ public class RepresentativeLine
 
     public LineString UpdateGeom()
     {
-        double bazierHandlerLength = 0.5f;
-        double rightShift = 0.1f;
-        var gf = new GeometryFactory();
+        double lengthRoughEstimate = from.Geom.Centroid.Distance(to.Geom.Centroid);
+        double bazierHandlerLength = 0.2d * lengthRoughEstimate;
+        double shiftRatio = 0.1f;
 
         Coordinate P0;
         Coordinate P1;
         Coordinate P2;
         Coordinate P3;
 
+        var gf = new GeometryFactory();
+
+        double fromShift = shiftRatio * from.Geom.Length;
         if (from.leftSpace == through)
         {
-            P0 = M.Translate(from.Geom.Centroid.Coordinate, from.P0.Coordinate, from.P1.Coordinate, rightShift);
+            P0 = M.Translate(from.Geom.Centroid.Coordinate, from.P0.Coordinate, from.P1.Coordinate, fromShift);
             Coordinate fromP0left = M.Rotate(from.P1.Coordinate, from.P0.Coordinate, Math.PI / 2.0f);
             P1 = M.Translate(P0, from.P0.Coordinate, fromP0left, bazierHandlerLength);
         }
         else if (from.rightSpace == through)
         {
-            P0 = M.Translate(from.Geom.Centroid.Coordinate, from.P1.Coordinate, from.P0.Coordinate, rightShift);
+            P0 = M.Translate(from.Geom.Centroid.Coordinate, from.P1.Coordinate, from.P0.Coordinate, fromShift);
             Coordinate fromP0right = M.Rotate(from.P1.Coordinate, from.P0.Coordinate, -Math.PI / 2.0f);
             P1 = M.Translate(P0, from.P0.Coordinate, fromP0right, bazierHandlerLength);
         }
         else throw new Exception("space contain the boundary but it is neighter the left nor the right side of boundary");
 
+        double toShift = shiftRatio * to.Geom.Length;
         if (to.leftSpace == through)
         {
-            P3 = M.Translate(to.Geom.Centroid.Coordinate, to.P1.Coordinate, to.P0.Coordinate, rightShift);
+            P3 = M.Translate(to.Geom.Centroid.Coordinate, to.P1.Coordinate, to.P0.Coordinate, toShift);
             Coordinate toP0left = M.Rotate(to.P1.Coordinate, to.P0.Coordinate, Math.PI / 2.0f);
             P2 = M.Translate(P3, to.P0.Coordinate, toP0left, bazierHandlerLength);
         }
         else if (to.rightSpace == through)
         {
-            P3 = M.Translate(to.Geom.Centroid.Coordinate, to.P0.Coordinate, to.P1.Coordinate, rightShift);
+            P3 = M.Translate(to.Geom.Centroid.Coordinate, to.P0.Coordinate, to.P1.Coordinate, toShift);
             Coordinate toP0right = M.Rotate(to.P1.Coordinate, to.P0.Coordinate, -Math.PI / 2.0f);
             P2 = M.Translate(P3, to.P0.Coordinate, toP0right, bazierHandlerLength);
         }
