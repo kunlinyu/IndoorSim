3739094095856e324b4bbfb535a6476b6825b716
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-07-20 08:16:52 +0800

    Change default tasks
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- Assets/src/controller/SimulationController.cs
+++ Assets/src/controller/SimulationController.cs
@@ -34,40 +34,45 @@ public class SimulationController : MonoBehaviour
                     indoorSimData.simulating = true;
                     indoorSimData.currentSimData.tasks.Clear();
 
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("0")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("1")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("2")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("3")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("4")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("5")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("6")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("7")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("8")}));
+                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() { new ActionMoveToContainer("9")}));
+
                     // indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() {
-                    //     new ActionMoveToContainer("123"),
+                    //     new ActionMoveToCoor(1.0f, 1.0f),
+                    //     new ActionMoveToCoor(-1.0f, 1.0f),
+                    //     new ActionMoveToCoor(1.0f, -1.0f),
+                    //     new ActionMoveToCoor(-1.0f, -1.0f),
+                    //     new ActionMoveToCoor(1.0f, 1.0f),
                     // }));
-                    // indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() {
-                    //     new ActionMoveToContainer("123"),
+                    // indoorSimData.currentSimData.tasks.Add(new ActionListTask(2.0d, new List<AgentAction>() {
+                    //     new ActionMoveToCoor(2.0f, 2.0f),
+                    //     new ActionMoveToCoor(-2.0f, 2.0f),
+                    //     new ActionMoveToCoor(2.0f, -2.0f),
+                    //     new ActionMoveToCoor(-2.0f, -2.0f),
+                    //     new ActionMoveToCoor(2.0f, 2.0f),
                     // }));
-                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(1.0d, new List<AgentAction>() {
-                        new ActionMoveToCoor(1.0f, 1.0f),
-                        new ActionMoveToCoor(-1.0f, 1.0f),
-                        new ActionMoveToCoor(1.0f, -1.0f),
-                        new ActionMoveToCoor(-1.0f, -1.0f),
-                        new ActionMoveToCoor(1.0f, 1.0f),
-                    }));
-                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(2.0d, new List<AgentAction>() {
-                        new ActionMoveToCoor(2.0f, 2.0f),
-                        new ActionMoveToCoor(-2.0f, 2.0f),
-                        new ActionMoveToCoor(2.0f, -2.0f),
-                        new ActionMoveToCoor(-2.0f, -2.0f),
-                        new ActionMoveToCoor(2.0f, 2.0f),
-                    }));
-                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(3.0d, new List<AgentAction>() {
-                        new ActionMoveToCoor(3.0f, 3.0f),
-                        new ActionMoveToCoor(-3.0f, 3.0f),
-                        new ActionMoveToCoor(-3.0f, -3.0f),
-                        new ActionMoveToCoor(3.0f, -3.0f),
-                        new ActionMoveToCoor(3.0f, 3.0f),
-                    }));
-                    indoorSimData.currentSimData.tasks.Add(new ActionListTask(4.0d, new List<AgentAction>() {
-                        new ActionMoveToCoor(4.0f, 4.0f),
-                        new ActionMoveToCoor(4.0f, -4.0f),
-                        new ActionMoveToCoor(-4.0f, -4.0f),
-                        new ActionMoveToCoor(-4.0f, 4.0f),
-                        new ActionMoveToCoor(4.0f, 4.0f),
-                    }));
+                    // indoorSimData.currentSimData.tasks.Add(new ActionListTask(3.0d, new List<AgentAction>() {
+                    //     new ActionMoveToCoor(3.0f, 3.0f),
+                    //     new ActionMoveToCoor(-3.0f, 3.0f),
+                    //     new ActionMoveToCoor(-3.0f, -3.0f),
+                    //     new ActionMoveToCoor(3.0f, -3.0f),
+                    //     new ActionMoveToCoor(3.0f, 3.0f),
+                    // }));
+                    // indoorSimData.currentSimData.tasks.Add(new ActionListTask(4.0d, new List<AgentAction>() {
+                    //     new ActionMoveToCoor(4.0f, 4.0f),
+                    //     new ActionMoveToCoor(4.0f, -4.0f),
+                    //     new ActionMoveToCoor(-4.0f, -4.0f),
+                    //     new ActionMoveToCoor(-4.0f, 4.0f),
+                    //     new ActionMoveToCoor(4.0f, 4.0f),
+                    // }));
 
                     simulation = new Simulation(indoorSimData.indoorData, indoorSimData.currentSimData, simulationView.GetAgentHWs());
                     timeScale = 1.0f;
