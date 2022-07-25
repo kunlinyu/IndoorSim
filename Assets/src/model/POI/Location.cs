a4e42b7c4c30342fa2545b8538b016a2d12fb431
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-07-25 10:00:25 +0800

    Add some method to Insert labels, descriptions and categories
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- Assets/src/model/POI/Location.cs
+++ Assets/src/model/POI/Location.cs
@@ -1,14 +1,15 @@
 // https://opengeospatial.github.io/poi/spec/poi-core.html
-namespace poi {
-
-public class Location : POIProperties
+namespace poi
 {
-    public Geometry point;
-    public Geometry line;
-    public Geometry polygon;
-    public POIProperties address;
-    public Relationship relationship;
-    public object undetermined;
-}
+
+    public class Location : POIProperties
+    {
+        public Geometry point = new Geometry();
+        public Geometry line = new Geometry();
+        public Geometry polygon = new Geometry();
+        public POIProperties address;
+        public Relationship relationship;
+        public object undetermined;
+    }
 
 }
--- Assets/src/model/POI/POI.cs
+++ Assets/src/model/POI/POI.cs
@@ -1,17 +1,76 @@
 using System.Collections.Generic;
 
 // https://opengeospatial.github.io/poi/spec/poi-core.html
-namespace poi {
-
-public class POI : POIProperties
+namespace poi
 {
-    public List<POIProperties> label = new List<POIProperties>();
-    public List<POIProperties> description = new List<POIProperties>();
-    public List<POIProperties> category = new List<POIProperties>();
-    public List<POIProperties> time = new List<POIProperties>();
-    public List<POIProperties> link = new List<POIProperties>();
-    public Location location;
-    public List<POIProperties> metadata = new List<POIProperties>();
-}
+
+    public class POI : POIProperties
+    {
+        public List<POIProperties> label = new List<POIProperties>();
+        public List<POIProperties> description = new List<POIProperties>();
+        public List<POIProperties> category = new List<POIProperties>();
+        public List<POIProperties> time = new List<POIProperties>();
+        public List<POIProperties> link = new List<POIProperties>();
+        public Location location;
+        public List<POIProperties> metadata = new List<POIProperties>();
+
+        public void AddLabel(string value) => AddLabel(value, "en-US", "primary");
+
+        /// <summary>
+        /// Add one Label to this POI
+        /// </summary>
+        /// <param name="value">this is the value string of the label</param>
+        /// <param name="lang">which language we use in the value string(https://www.ietf.org/rfc/rfc3066.txt). For example: en-US, en-GB, en-AU, zh-CN, </param>
+        /// <param name="term">"primary" or "note"</param>
+        public void AddLabel(string value, string lang, string term)
+        {
+            var label = new poi.POIProperties()
+            {
+                value = value,
+                lang = lang,
+                term = term,
+            };
+
+            this.label.Add(label);
+        }
+
+        public void AddDescription(string value) => AddDescription(value, "en-US");
+        public void AddDescription(string value, string lang)
+        {
+            var description = new poi.POIProperties()
+            {
+                value = value,
+                lang = lang,
+            };
+
+            this.description.Add(description);
+        }
+
+
+        public void AddCategory(string term) => AddCategory(term, "free", "");
+
+        /// <summary>
+        ///     Add Category
+        /// </summary>
+        /// <param name="term">keyword to specify the category</param>
+        /// <param name="scheme">classification scheme to which the value belongs</param>
+        /// <param name="value">describe human readable categorical</param>
+        public void AddCategory(string term, string scheme, string value)
+        {
+            var category = new poi.POIProperties()
+            {
+                term = term,
+                scheme = new System.Uri(scheme),
+                value = value,
+            };
+
+            this.category.Add(category);
+        }
+
+        public void AddPointLocation(Geometry location) => this.location.point = location;
+        public void AddLineLocation(Geometry location) => this.location.line = location;
+        public void AddPolygonLocation(Geometry location) => this.location.polygon = location;
+
+    }
 
 }
