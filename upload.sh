e77b6b09bfd1ca3ec1e662e2f6d16f2ecc6f111d
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-08-03 15:02:54 +0800

    Update upload script to release webgl version
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>
    Change-Id: I77fe5f609d479b2fd3e5812f6c8e2ca760fa6dbb
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- upload.sh
+++ upload.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-aws s3 cp index.html s3://indoorsim/index.html
-aws s3 cp --recursive ./TemplateData s3://indoorsim/TemplateData
-aws s3 cp --recursive ./StreamingAssets s3://indoorsim/StreamingAssets
 
-aws s3 cp Build/IndoorSim-release.data.gz         s3://indoorsim/Build/IndoorSim-release.data.gz         --content-encoding=gzip
-aws s3 cp Build/IndoorSim-release.framework.js.gz s3://indoorsim/Build/IndoorSim-release.framework.js.gz --content-encoding=gzip --content-type=application/javascript
-aws s3 cp Build/IndoorSim-release.loader.js       s3://indoorsim/Build/IndoorSim-release.loader.js                               --content-type=application/javascript
-aws s3 cp Build/IndoorSim-release.wasm.gz         s3://indoorsim/Build/IndoorSim-release.wasm.gz         --content-encoding=gzip --content-type=application/wasm
+if [ $# -eq 0 ]
+then
+  echo "Usage upload.sh IndoorSim-Release-V0.0.0"
+fi
+
+if [ -d "$1" ]
+then
+  RELEASEPATH=$1
+else
+  echo "Directory don't exist"
+  exit
+fi
+
+aws s3 cp index.html s3://indoorsim/index.html
+
+echo $RELEASEPATH
+
+aws s3 cp $RELEASEPATH/index.html s3://indoorsim/$RELEASEPATH/index.html
+
+aws s3 cp --recursive $RELEASEPATH/TemplateData s3://indoorsim/$RELEASEPATH/TemplateData
+aws s3 cp --recursive $RELEASEPATH/StreamingAssets s3://indoorsim/$RELEASEPATH/StreamingAssets
+
+aws s3 cp $RELEASEPATH/Build/$RELEASEPATH.data.gz         s3://indoorsim/$RELEASEPATH/Build/$RELEASEPATH.data.gz         --content-encoding=gzip
+aws s3 cp $RELEASEPATH/Build/$RELEASEPATH.framework.js.gz s3://indoorsim/$RELEASEPATH/Build/$RELEASEPATH.framework.js.gz --content-encoding=gzip --content-type=application/javascript
+aws s3 cp $RELEASEPATH/Build/$RELEASEPATH.loader.js       s3://indoorsim/$RELEASEPATH/Build/$RELEASEPATH.loader.js                               --content-type=application/javascript
+aws s3 cp $RELEASEPATH/Build/$RELEASEPATH.wasm.gz         s3://indoorsim/$RELEASEPATH/Build/$RELEASEPATH.wasm.gz         --content-encoding=gzip --content-type=application/wasm
