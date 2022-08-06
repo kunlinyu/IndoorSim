6b6f8af51cb3724d6df0b54bf130399c3c4d6677
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-08-06 14:11:31 +0800

    Add BuildPlayer to build automatically
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>
    Change-Id: I4be122b6c3930a37e66703a61259d903d0a3dc80

---
+++ Assets/src/BuildPlayer.cs
@@ -0,0 +1,50 @@
+#if UNITY_EDITOR
+
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayer : MonoBehaviour
+{
+    [MenuItem("Build/Build Linux")]
+    public static void BuildLinux() => Build("../linux64", BuildTarget.StandaloneLinux64, true);
+
+    [MenuItem("Build/Build WebGL")]
+    public static void BuildWebGL() => Build("../webgl", BuildTarget.WebGL, true);
+
+
+    public static void Build(string prefix, BuildTarget target, bool development)
+    {
+        string[] lines = File.ReadAllLines(".git/refs/heads/master");
+        if (lines.Length < 1)
+            throw new System.Exception("can not read line from file");
+
+        string shortSHA1 = lines[0].Substring(0, 7);
+
+        string dirName = "IndoorSim-" + target.ToString() + (development ? "-dev" : "") + "-V" + Application.version + "." + shortSHA1;
+        Debug.Log(dirName);
+        if (target == BuildTarget.StandaloneLinux64)
+            dirName = dirName + "/" + dirName;
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/MappingScene.unity" };
+        buildPlayerOptions.locationPathName = prefix + "/" + dirName;
+        buildPlayerOptions.target = target;
+        buildPlayerOptions.options = development ? BuildOptions.Development : BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes into path " + summary.outputPath);
+        else
+            Debug.Log("Build " + summary.result.ToString());
+    }
+
+
+}
+
+#endif
