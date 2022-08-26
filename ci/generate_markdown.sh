3c9d3a7e14b924943f7adf628392ff96958c75cb
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-08-26 09:31:48 +0800

    Add html template to solve the dependency problem
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>
    Change-Id: Ifaf1945e49df8eb4b8d1aaacd1bd7bae3d874285

--- ci/generate_markdown.sh
+++ ci/generate_markdown.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
-pandoc ../RELEASE.md -f markdown -o index.html --template=${HOME}/.pandoc/templates/easy_template.html --toc --metadata pagetitle="IndoorSim release"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+
+pandoc ../RELEASE.md -f markdown -o index.html --template=${CURRENT_DIR}/easy_template.html --toc --metadata pagetitle="IndoorSim release"
