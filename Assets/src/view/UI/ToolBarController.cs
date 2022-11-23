a46aced8ab5604680948a4d94eb7e6dbc05c76d7
Kunlin Yu <yukunlin@syriusrobotics.com>
2022-11-23 11:05:34 +0800

    Do not use button data but edit in ui builder
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- Assets/src/view/UI/ToolBarController.cs
+++ Assets/src/view/UI/ToolBarController.cs
@@ -10,70 +10,54 @@ public class ToolBarController : MonoBehaviour
     [SerializeField] private VisualTreeAsset m_ToolBarButtonTemplate;
     private Button activeButton = null;
 
-    private Dictionary<Button, ToolButtonData> button2ButonData = new Dictionary<Button, ToolButtonData>();
-
     void Start()
     {
         VisualElement toolBar = rootUIDocument.rootVisualElement.Q<VisualElement>("ToolBar");
         LoadButtons(toolBar,
-            (tbd) => { eventDispatcher.Raise(this, new UIEvent() { name = tbd.m_ToolName, message = "trigger", type = UIEventType.ToolButton }); },
-            (tbd) => { eventDispatcher.Raise(this, new UIEvent() { name = tbd.m_ToolName, message = "enter", type = UIEventType.ToolButton }); },
-            (tbd) => { eventDispatcher.Raise(this, new UIEvent() { name = tbd.m_ToolName, message = "leave", type = UIEventType.ToolButton }); });
-        toolBar.RegisterCallback<MouseEnterEvent>(e =>
-            { eventDispatcher.Raise(toolBar, new UIEvent() { name = "tool bar", message = "enter", type = UIEventType.EnterLeaveUIPanel }); });
-        toolBar.RegisterCallback<MouseLeaveEvent>(e =>
-            { eventDispatcher.Raise(toolBar, new UIEvent() { name = "tool bar", message = "leave", type = UIEventType.EnterLeaveUIPanel }); });
+            (toolName) => { eventDispatcher.Raise(this, new UIEvent() { name = toolName, message = "trigger", type = UIEventType.ToolButton }); },
+            (toolName) => { eventDispatcher.Raise(this, new UIEvent() { name = toolName, message = "enter", type = UIEventType.ToolButton }); },
+            (toolName) => { eventDispatcher.Raise(this, new UIEvent() { name = toolName, message = "leave", type = UIEventType.ToolButton }); });
     }
-    public void LoadButtons(VisualElement toolBar, Action<ToolButtonData> onTrigger, Action<ToolButtonData> onEnter, Action<ToolButtonData> onLeave)
+    public void LoadButtons(VisualElement toolBar, Action<string> onTrigger, Action<string> onEnter, Action<string> onLeave)
     {
-        List<ToolButtonData> allToolButton = new List<ToolButtonData>(Resources.LoadAll<ToolButtonData>("ToolButtonData"));
-        allToolButton.Sort((tbd1, tbd2) => { return tbd1.m_SortingOrder - tbd2.m_SortingOrder; });
-
-        foreach (ToolButtonData tbd in allToolButton)
+        toolBar.Query<Button>().ForEach(button =>
         {
-            TemplateContainer buttonContainer = m_ToolBarButtonTemplate.Instantiate();
-            Button button = buttonContainer.Q<Button>("ToolBarButton");
-
-            button2ButonData[button] = tbd;
-
-            button.text = "";
-            button.style.backgroundImage = new StyleBackground(tbd.m_PortraitImage);
-            button.tooltip = tbd.m_ToolName;
             button.AddManipulator(new ToolTipManipulator(rootUIDocument.rootVisualElement));
-            if (tbd.m_Class == ToolType.TriggerAndFinish)
-            {
-                button.clicked += () =>
-                {
-                    onTrigger?.Invoke(tbd);
-                    if (activeButton != null)
-                        onLeave?.Invoke(button2ButonData[activeButton]);
-                    DisableButton(activeButton);
-                    toolBar.Focus();  // focus on toolbar to release focus on button
-                };
-            }
-            else if (tbd.m_Class == ToolType.EnterActiveMode)
-            {
-                button.clicked += () =>
-                {
-                    if (activeButton == button)
-                    {
-                        onLeave?.Invoke(tbd);
-                        DisableButton(button);
-                    }
-                    else
-                    {
-                        onEnter?.Invoke(tbd);
-                        SwitchButton(button);
-                    }
-                    toolBar.Focus();  // focus on toolbar to release focus on button
-                };
+            button.RegisterCallback<MouseEnterEvent>(e =>
+            { eventDispatcher.Raise(toolBar, new UIEvent() { name = "tool bar", message = "enter", type = UIEventType.EnterLeaveUIPanel }); });
+            button.RegisterCallback<MouseLeaveEvent>(e =>
+            { eventDispatcher.Raise(toolBar, new UIEvent() { name = "tool bar", message = "leave", type = UIEventType.EnterLeaveUIPanel }); });
+        });
 
-            }
-            else throw new Exception("unknow ToolType: " + tbd.m_Class);
+        toolBar.Query<Button>("triggerButton").ForEach(button =>
+        {
+            button.clicked += () =>
+            {
+                onTrigger?.Invoke(button.tooltip);
+                if (activeButton != null)
+                    onLeave?.Invoke(button.tooltip);
+                DisableButton(activeButton);
+                toolBar.Focus();  // focus on toolbar to release focus on button
+            };
+        });
 
-            button.tabIndex = -1;
-            toolBar.Add(buttonContainer);
-        }
+        toolBar.Query<Button>("enterLeaveButton").ForEach(button =>
+        {
+            button.clicked += () =>
+            {
+                if (activeButton == button)
+                {
+                    onLeave?.Invoke(button.tooltip);
+                    DisableButton(button);
+                }
+                else
+                {
+                    onEnter?.Invoke(button.tooltip);
+                    SwitchButton(button);
+                }
+                toolBar.Focus();  // focus on toolbar to release focus on button
+            };
+        });
     }
 
     private void SwitchButton(Button button)
--- Assets/src/view/UI/ViewPanelController.cs
+++ Assets/src/view/UI/ViewPanelController.cs
@@ -11,31 +11,25 @@ public class ViewPanelController : MonoBehaviour
     void Start()
     {
         VisualElement viewPanel = rootUIDocument.rootVisualElement.Q<VisualElement>("ViewPanel");
-        List<ToolButtonData> allToolButton = new List<ToolButtonData>(Resources.LoadAll<ToolButtonData>("ViewButtonData"));
-        foreach (ToolButtonData tbd in allToolButton)
+
+        viewPanel.Query<Button>().ForEach(button =>
         {
-            TemplateContainer buttonContainer = m_ViewBarButtonTemplate.Instantiate();
-            Button button = buttonContainer.Q<Button>("ToolBarButton");
-            button.text = "";
-            button.style.backgroundImage = new StyleBackground(tbd.m_PortraitImage);
-            button.tooltip = tbd.m_ToolName;
             button.AddManipulator(new ToolTipManipulator(rootUIDocument.rootVisualElement));
             button.AddToClassList("enable");
             button.clicked += () =>
             {
                 button.ToggleInClassList("enable");
                 if (button.ClassListContains("enable"))
-                    eventDispatcher.Raise(this, new UIEvent() { name = tbd.m_ToolName, message = "enable", type = UIEventType.ViewButton });
+                    eventDispatcher.Raise(this, new UIEvent() { name = button.tooltip, message = "enable", type = UIEventType.ViewButton });
                 else
-                    eventDispatcher.Raise(this, new UIEvent() { name = tbd.m_ToolName, message = "disable", type = UIEventType.ViewButton });
+                    eventDispatcher.Raise(this, new UIEvent() { name = button.tooltip, message = "disable", type = UIEventType.ViewButton });
                 viewPanel.Focus();  // focus on viewBar to release focus on button
             };
-            button.tabIndex = -1;
-            viewPanel.Add(buttonContainer);
-        }
-        viewPanel.RegisterCallback<MouseEnterEvent>(e =>
+            button.RegisterCallback<MouseEnterEvent>(e =>
             { eventDispatcher.Raise(this, new UIEvent() { name = "view panel", message = "enter", type = UIEventType.EnterLeaveUIPanel }); });
-        viewPanel.RegisterCallback<MouseLeaveEvent>(e =>
+            button.RegisterCallback<MouseLeaveEvent>(e =>
             { eventDispatcher.Raise(this, new UIEvent() { name = "view panel", message = "leave", type = UIEventType.EnterLeaveUIPanel }); });
+        });
+
     }
 }
